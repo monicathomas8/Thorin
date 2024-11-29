fa53c834bc4c2af2eb1e9e54caef530e304a2d7b
Monica Thomas <monicathomas8@icloud.com>
2024-11-29 11:36:30 +0000

    advanced routing in run.py line 22 and about for loops line 19

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, render_template
 
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -16,7 +16,18 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About",company=data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact")
@@ -30,7 +41,7 @@ def careers():
 
 
 if __name__ == "__main__":
-    app.run (
+    app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
